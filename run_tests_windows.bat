e7aa8cca2c3366ff7f8f1443d9d7e8ce5a0d1522
gorash-bolatu <cotwi10@yandex.ru>
2025-05-30 17:26:10 +0800

    scripts

---
+++ run_tests_windows.bat
@@ -0,0 +1,3 @@
+javac -classpath ".;junit-platform-console-standalone-1.7.0-M1.jar" *.java
+java -classpath ".;junit-platform-console-standalone-1.7.0-M1.jar" org.junit.runner.JUnitCore Lab11_Tests
+pause
